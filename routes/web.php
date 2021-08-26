a4b9af08f3927a2a7906279e5e21c156e329d549
Risa Hirata <89110376+RisaHirata@users.noreply.github.com>
2021-08-26 09:59:41 +0900

    修正

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,8 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
 //課題4用
-   Route::get('admin/profile/create', 'Admin\ProfileController@add')
-   Route::get('admin/profile/edit', 'Admin\ProfileConroller@edit')
+   Route::get('admin/profile/create', 'Admin\ProfileController@add');
+   Route::get('admin/profile/edit', 'Admin\ProfileConroller@edit');
 
 //課題3用
 Route::get('XXX', 'Admin\AAAController@bbb');

c8eed188938065e59b305eaef860f5350cb4c7df
Risa Hirata <89110376+RisaHirata@users.noreply.github.com>
2021-09-17 08:59:41 +0900

    13

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit'); 
     Route::post('profile/create', 'Admin\ProfileController@create');
-    Route::post('profile/edit', 'Admin\ProfileConroller@update');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
@@ -43,4 +43,5 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
     Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
     Route::get('news/delete', 'Admin\NewsController@delete')->middleware('auth');
+    Route::get('/', 'NewsController@index');
 });
